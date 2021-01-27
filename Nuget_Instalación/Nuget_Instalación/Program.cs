1f02910a9213979a8a2d2c9b6a4e21a37cc4bc13
Steelnox <eric14pastor@gmail.com>
2021-01-27 10:59:37 +0100

    Instalacion del nuget realizada

---
+++ Nuget_Instalación/Nuget_Instalación/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nuget_Instalación
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
